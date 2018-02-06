83b7b1b1844c5a4f338e0e71ea196755913917ed
Cristian Sulea <cristian.sulea.79@gmail.com>
2018-02-06 15:22:08 +0200

    C Language exe/launcher

---
+++ resources/exe/JaToo Imager.c
@@ -0,0 +1,51 @@
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <process.h> 
+ 
+int main(int argc, char *argv[]) {
+/*
+char * cp = getenv("JATOO_IMAGER_PATH");
+strcat(cp, "\\lib\\*");
+printf("The argument supplied is %s\n", cp);
+spawnlp( P_WAIT, "javaw.exe", "javaw.exe", "-cp", cp, "jatoo.imager.JaTooImagerLauncher", NULL );
+*/
+
+char param[255];
+strcpy(param, "\"");
+strcat(param, argv[1]);
+strcat(param, "\"");
+
+spawnlp( P_WAIT, "launcher.exe", "launcher.exe", param, NULL );
+
+	if( argc == 2 ) {
+		
+		char * userFolder = getenv("UserProfile");
+		char * fileFolder1 = "\\.jatoo";
+		char * fileFolder2 = "\\.imager";
+		char * fileFolder3 = "\\images";
+		
+		char file[255];
+		strcpy(file, userFolder);
+		
+		strcat(file, fileFolder1);
+		mkdir(file);
+		strcat(file, fileFolder2);
+		mkdir(file);
+		strcat(file, fileFolder3);
+		mkdir(file);
+		
+		char fileName[255];
+		tmpnam(fileName);
+		
+		strcat(file, fileName);
+		
+		FILE * f = fopen(file, "w");
+		fprintf(f, argv[1]);
+		fclose(f);	
+	}
+	
+	return 0;
+}
---
+++ resources/exe/build.bat
@@ -0,0 +1,3 @@
+windres icon.rc icon.o
+gcc -mwindows icon.o "JaToo Imager.c" -o "JaToo Imager.exe"
+pause
